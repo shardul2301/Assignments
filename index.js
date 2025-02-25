b48f7b0e15af44173e85c7930e3b811af1cfdefc
SHARDUL MANE <105095567+shardul2302@users.noreply.github.com>
2025-02-25 10:05:46 +0530

    the difference beyween let vs const vs var

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Hello, World!");
+
+console.log('before var initailzation is :',a);
+var a = 23;
+console.log('after  var initailzation is :',a);
+let b ;
+b=56;
+const s=34;
+console.log('the  vaule of let ',s);
+console.log('the  vaule of let ',b);
