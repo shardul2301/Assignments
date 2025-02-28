0f60f3e9b6d10e125ff9206a464d6c24d2c958c2
SHARDUL MANE <105095567+shardul2302@users.noreply.github.com>
2025-02-28 17:10:07 +0530

    type conversion problems

---
+++ practies28feb2025.js
@@ -0,0 +1,71 @@
+//Online Javascript Editor for free
+//Write, Edit and Run your Javascript code using JS Online Compiler
+//type conversion in js 
+console.log("Try programiz.pro");
+let web = "0";
+let web2 ="0";
+let web3 = "uy";
+
+web= Number(web);
+web2= String(web2);
+web3= Boolean(web3);
+console.log( web ,typeof web);
+console.log( web2 ,typeof web2);
+console.log( web3 ,typeof web3);
+let date = new Date();
+console.log(date,typeof date);
+let stri = Number('234');
+console.log(stri,typeof stri);
+stri = String(456)
+console.log(stri,typeof stri);
+stri = Number('45f6')
+console.log(stri,typeof stri);
+
+
+stri = parseFloat('450.09')
+console.log(stri,typeof stri);
+stri = parseInt('450.09')
+console.log(stri,typeof stri);
+console.log('67'+ 13 - 6*2);
+console.log(90 +0=='90');
+/////////////////////////////////////
+
+
+console.log( '5' / '5')
+console.log('10' - 5 + 2);
+console.log(('5' * '3') == (true + true));
+let x = '8' - 4;
+console.log(x, typeof x);
+console.log('7' + 3 - '2');
+console.log(('4' - '1' + '6') * false);
+console.log(-'9');
+let result = '10' - - '4';
+console.log(result, typeof result);
+console.log('6' + 3 * 2 - '4');
+console.log(('8' - '4' + '3') * false);
+console.log(89- -'12');
+let num = '7' - '3' * 2;
+console.log(num, typeof num);
+console.log(('5' * '2' - '1') == (true + true + true));
+console.log('9' + 5 * 2 - '4' * true);
+console.log(+'10' - 5 );
+console.log(10 - 5 + 6);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
