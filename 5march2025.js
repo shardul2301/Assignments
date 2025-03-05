7e20451ba65ea349992d11987daa891081ed7c34
SHARDUL MANE <105095567+shardul2302@users.noreply.github.com>
2025-03-05 12:41:38 +0530

    Add files via upload

---
+++ 5march2025.js
@@ -0,0 +1,30 @@
+function fun(r){
+  for(let i = 0 ; i<r;i++)
+  {
+    
+    let   row = '';
+    for (let j = 0 ; j <i;j++){
+        row = row + " *";
+        
+    }
+    console.log(row); // console.log(" ")
+//   break;/
+}
+}
+fun(10);
+console.log("Try programiz.pro");
+//let n = 3;
+function fun2(r){
+  for(let i = 0 ; i<r;i++)
+  {
+    
+    let   row = '';
+    for (let j = 0 ; j <r;j++){
+        row = row + " *";
+        
+    }
+    console.log(row); // console.log(" ")
+//   break;/
+}
+}
+fun2(10);
