3e0ff29956c76ab36cb49237fd0f19572a2bb233
SHARDUL MANE <105095567+shardul2302@users.noreply.github.com>
2025-03-06 18:09:39 +0530

    Add files via upload

---
+++ 6march2025.js
@@ -0,0 +1,18 @@
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const citrus = fruits.slice(1, 3);
+console.log(citrus);
+console.log("Try programiz.pro");
+console.log("Hello, World!");
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let size = fruits.length;
+console.log(fruits[2]);
+const fruitss = ["Banana", "Orange", "Apple", "Mango",'shshshs'];
+let fruite = fruitss.at(3);
+console.log(fruite)
+console.log(fruitss.pop[1])
+console.log(fruitss)
+// console.log(fruitss.pop())
+console.log(fruitss)
+// console.log(fruitss.pop())
+// console.log(fruitss.pop())
+console.log(fruitss)
