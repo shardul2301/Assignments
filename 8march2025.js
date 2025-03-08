1b137ea55551b0fbf37cd7f6bf88916571dfa052
SHARDUL MANE <105095567+shardul2302@users.noreply.github.com>
2025-03-08 09:51:26 +0530

    Create 8march2025.js
    for loops types

---
+++ 8march2025.js
@@ -0,0 +1,47 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+// for loops types 
+console.log("Try programiz.pro");
+let obj= {
+    val1:"hs",
+    val2:"skashdk"
+}
+for(let items in obj){
+    console.log(items);
+}
+
+// aaray from 
+let name = 'shardul';
+let arr = Array.from(name);
+console.log(arr);
+
+
+// for...of
+let num = [1,5,8,9,3];
+for (let items of num){
+    console.log(items);
+}
+
+// for in 
+for (let i in num){
+    console.log(i);
+}
+// output
+// Try programiz.pro
+// val1
+// val2
+// [
+//   's', 'h', 'a',
+//   'r', 'd', 'u',
+//   'l'
+// ]
+// 1
+// 5
+// 8
+// 9
+// 3
+// 0
+// 1
+// 2
+// 3
+// 4
