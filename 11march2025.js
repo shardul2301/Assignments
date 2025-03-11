69f42c9cb9ccb4d9d2945144e8abf2b74ba2d37b
SHARDUL MANE <105095567+shardul2302@users.noreply.github.com>
2025-03-11 19:07:19 +0530

    Create 11march2025.js

---
+++ 11march2025.js
@@ -0,0 +1,29 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+console.log("Try programiz.pro");
+var rop = 'shardul';
+console.log(rop)
+let wep = 23;
+console.log(wep);
+const  arr = [1,5,8,9,2,3];
+console.log(arr);
+ 
+ 
+ {
+     var internal = 546;
+ }
+ console.log(internal)//global scope 
+ {
+     let top =  978;//let is block scope 
+     
+     
+ }
+//  console.log(top)
+
+{
+    const ty = 'ratan tata'; //const is block scope 
+    console.log(ty);
+}
+
+//java script is programmming language not use for creating  the action on broswer firstly  
