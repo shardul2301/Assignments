442675c250af0749e8596e009851da7f4387dd82
SHARDUL MANE <105095567+shardul2302@users.noreply.github.com>
2025-03-17 12:18:25 +0530

    Create 17march2025.js
    falsy vaules in js

---
+++ 17march2025.js
@@ -0,0 +1,11 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+console.log("Try programiz.pro");
+console.log("falsy vaules in js ")
+console.log(Boolean(0));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(""));
+// console.log(Boolean(NAN));
+console.log(Boolean(false));
