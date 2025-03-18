dd7a2e8e44fb9b9491ef1abc5ca788a34e637195
SHARDUL MANE <105095567+shardul2302@users.noreply.github.com>
2025-03-18 16:12:35 +0530

    Update 18march2025.js
    implicit and explicit type conversions quetions

--- 18march2025.js
+++ 18march2025.js
@@ -57,6 +57,65 @@ function updated(tower){
 
 const updatednumber = updated(Building);
 
+
+console.log("implicit and explicit type conversions");
+// q1
+let a = 5.7;
+let b = parseInt(a);
+console.log(b);
+// q2
+let x = String(123);
+console.log(typeof x);
+// q3
+console.log(parseFloat("3.14") + 2);
+// q4
+let num = Number("123abc");
+console.log(num);
+// q5
+let x1 = 10;
+let y = "20";
+let z = String(x1) + y;
+console.log(z);
+// q6
+console.log(Number(true), Number(false));
+// q7
+console.log(Boolean("false"));
+// q8
+let arr = [1, 2, 3];
+console.log(String(arr));
+// q9
+console.log(parseInt(3.99));
+// q10
+let mySet = new Set([1, 2, 3]);
+let myArray = Array.from(mySet);
+console.log(myArray);
+// q11
+console.log(+"10" + 5);
+// q12
+let x2 = "5";
+let y1 = 2;
+console.log(x2 * y1);
+
+// q13
+console.log(Number(null));
+// q14
+console.log(Boolean([]));
+// q15
+console.log(String(undefined), typeof String(undefined));
+// q16
+console.log("5" - - "2");
+// q17
+console.log(Number([]), Number({}));
+// q18
+console.log(true + false + "3");
+// q19
+console.log(parseInt("12px"), parseFloat("12.5px"));
+// q20
+let a1 = "1";
+let b1 = 2;
+console.log(a1 + b1, a1 - b1);
+
+
 console.log(Building) //so in this out put number of pepoles are update bucause of object are not passby vaule it pass by reference
  
  
