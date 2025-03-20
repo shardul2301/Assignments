235834702bea7480a199a6abdb931ae54cfd76b1
SHARDUL MANE <105095567+shardul2302@users.noreply.github.com>
2025-03-20 16:16:19 +0530

    Create 20march2025.js

---
+++ 20march2025.js
@@ -0,0 +1,104 @@
+
+console.log("Hello, World!");
+
+// destrucring aaray
+const destrucring = ['shardul',67,54];
+let username =destrucring[0] 
+console.log(username);
+let [a,b,c]= destrucring;
+console.log(a);
+console.log(b);
+console.log(c);
+// const emty = [ , , , , , ,7 , , , , ]
+// console.log(emty)
+function user(){
+    return ['prapti',18,'delhi'];
+}
+let [name,age,city]=user();
+console.log(name);
+console.log(city);
+// Destructuring Object 
+const car = {
+    carname : ' Tata neno',
+    colour: 'red',
+    price: 230000
+    
+    }
+const {carname,colour,price} = car;
+console.log(`the car name is ${carname}  and colour is ${colour} ,discounted price for sale is ${price}`)
+//   qq 
+ const animal = {
+    animalname : ' lepord',
+    pcolour: 'black',
+    area :'jungle'
+    
+    }
+const {animalname, area} = animal;  
+console.log(animalname);
+// console.log(pcolour);//SyntaxError: Unexpected token ','
+console.log(area);
+//it will not automaticaaly ingonrr the blanck space as in aary we can do this 
+const productDetails={
+    productname :"skybag",
+    category :"stationary",
+    type :"treking bag",
+    
+}
+const { productname : bagname,category,type }= productDetails;
+console.log(bagname);
+console.log(`the category of this bag is ${category} and type is ${type}`)
+// qq
+const laptop = {
+    brand: "Apple",
+    model: "MacBook Air",
+    price1: 999
+};
+
+let {brand,model,price1} = laptop;
+console.log(brand);
+console.log(model);
+console.log(price1);
+//  qq
+ const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25
+};
+let {firstName:fname,lastName:lname } = person;
+console.log(fname);
+console.log(lname);
+
+const address = {
+    city1: "New York"
+};
+
+let {city1,country="usa"}=address;
+console.log(city1);
+console.log(country);
+// qq
+  const user1 = {
+    namee: "Alice",
+    address1: {
+        street: "123 Main St",
+        zipcode: 10001
+    }
+};
+
+let {address1:{street,zipcode}} =user1;
+console.log(street)
+  console.log(zipcode)
+//  qq   
+const productDetail = {
+    productName: "Nike Shoes",
+    category: "Footwear",
+    price: 120
+};
+
+function displayProduct(product) {
+  let   {productName,category} = product;
+  console.log('product:',productName);
+   console.log('category:',category);
+    return ;
+}
+
+ displayProduct(productDetail);
