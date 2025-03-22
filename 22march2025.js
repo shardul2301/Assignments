402b2edf035711aaacf94ca9b4cfd1ec6ff6aeda
SHARDUL MANE <105095567+shardul2302@users.noreply.github.com>
2025-03-22 17:13:25 +0530

    Update 22march2025.js

--- 22march2025.js
+++ 22march2025.js
@@ -46,3 +46,96 @@ console.log(admin.exam)
 delete admin.centerno
 console.log(admin);
 
+let array =[2,1,5,6,4,9,43,42];
+let result = array.filter((num)=>{
+    return num%2===0;
+    
+})
+console.log(result);
+
+
+// JavaScript is awesome
+ let string = 'JavaScript is awesome';
+ const res = string.slice(13)
+ console.log(res);
+ 
+ 
+ let arr = ['prapti','rahi','pradyna','aditya'];
+ for(let values of arr){
+     console.log(values)
+ }
+ 
+ let reducearr= [10, 20, 30, 40];
+ const reduceans= reducearr.reduce((acc,curr)=>{
+     return acc+curr;
+ });
+ console.log( 'the sum of element is :',reduceans);
+ //output based 
+ console.log(a);
+var a = 5;
+// console.log(b);
+let b = 10;
+
+
+function  falsyprint(falsy){
+    for(let i = 0 ; i<falsy.length;i++){
+        // console.log(i)
+        // console.log(falsy[i]);
+        let point=[];
+      if(falsy[i] == 0 ||falsy[i] == '' || falsy[i] == null || falsy[i] == undefined || falsy[i] == false ){
+        console.log(falsy[i]);
+    
+    }
+    }
+    
+}
+let falsy = [0,null,undefined,false,'hi',34,'shardul','false'];
+const op = falsyprint(falsy);
+ 
+ let name = "Alice";
+let age = 25;
+console.log(`My name is  ${name}  and I am ${age}  years old.`);
+//  output based 
+console.log("5" - 3);
+console.log("5" + 3);
+console.log("5" * "2");
+console.log(true + false);
+
+
+function evenodd(num){
+    if(num%2==0){
+        return 'even';
+    }
+    return 'odd';
+}
+console.log(evenodd(27))
+ console.log(evenodd(72))
+ 
+//  output based 
+console.log("Start");
+setTimeout(() => console.log("Timeout"), 0);
+console.log("End");
+
+
+function outer() {
+    let x = 10;
+    function inner() {
+        console.log(x);
+    }
+    return inner;
+}
+let fn = outer();
+fn();
+
+
+for (var i = 0; i < 3; i++) {
+    setTimeout(() => console.log(i), 1000);
+}
+
+ const sum = (a,b)=>{
+     return a+b
+ }
+ console.log('the sum of a , b is :',sum(2,5))
+ 
+ 
+ 
