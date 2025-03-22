83c5475359e006269d3f47428513d1861da218bd
SHARDUL MANE <105095567+shardul2302@users.noreply.github.com>
2025-03-22 12:50:30 +0530

    Create 22march2025.js

---
+++ 22march2025.js
@@ -0,0 +1,23 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+console.log(typeof (undefined));
+console.log(typeof ([3,5,7]))
+console.log(typeof null);
+
+console.log(typeof 'Bigint')
+console.log(typeof(67))
+let bigInt = 445354645654655555556n
+console.log(typeof(bigInt));
+console.log(typeof('@'));
+console.log(typeof("5"-2));
+console.log(typeof("5"+2));
+console.log(Number("5") + 3);  
+console.log(String(100));      
+console.log(Boolean(0));      
+console.log(parseInt("10px"));
+console.log(Number("10px"));
+console.log("5" + 3);    
+console.log("5" - 3);  
+console.log(5 == "5");  
+console.log(true + 1);
