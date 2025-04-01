e9bf2ae7c153d3cf6dbd1d628c3b3754ded37e16
shardul2301 <shardul.mane@nimapinfotech.com>
2025-04-01 18:57:28 +0530

    Update 1april2025.js

--- 1april2025.js
+++ 1april2025.js
@@ -26,3 +26,54 @@ function maxdivisor(num1,num2){
     return divisor;
 }
 console.log(`the answer is : ${maxdivisor(num1,num2)}`);
+
+let string =  'asdfg fddef';
+function reversee(string){
+let reverse= "";
+for(let i = string.length-1; i>=0;i--){
+//   console.log(i)
+    reverse = reverse+string[i]
+}
+return reverse;
+}
+
+ const res = reversee('shardul hskhfksdhfk');
+ console.log(res);
+
+const e = 10;
+function sum(a) {
+  return function (b) {
+    return function (c) {
+     
+      return function (d) {
+        
+        return a + b + c + d + e;
+      };
+    };
+  };
+}
+
+console.log(sum(1)(2)(3)(4)); // 20
+
+function makeAdder(x) {
+  return function (y) {
+    return x + y;
+  };
+}
+
+const add5 = makeAdder(5);
+const add10 = makeAdder(10);
+
+console.log(add5(2)); // 7
+console.log(add10(2)); // 12
+
+function makeFunc() {
+  const name = "code";
+  function displayName() {
+    console.log(name);
+  }
+  return displayName;
+}
+
+const myFunc = makeFunc();
+myFunc();
