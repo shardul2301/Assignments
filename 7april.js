6c425e8c8a3f6c737ccceaf48cdbda20c80f8f98
shardul2301 <shardul.mane@nimapinfotech.com>
2025-04-07 18:32:11 +0530

    Update 7april.js

--- 7april.js
+++ 7april.js
@@ -3,18 +3,16 @@ const math = require('./math.js');
 
 const result = math.add(3, 4); 
 console.log(result); 
-// demo.js
-// function add(a,b){
-//     return a+b;
-// }
-// module.exports.add = add ;
+demo.js
+function add(a,b){
+    return a+b;
+}
+module.exports.add = add ;
 // math.js
 function add(a, b) {
     return a + b;  
   }
-  
-  
-  module.exports.add = add;
+ module.exports.add = add;
 
   // index.js
 var b = 'https//:mylog.io/log' ;
@@ -38,5 +36,36 @@ const math = require('./demo.js')
 // ans('shardul')
 const result = math.add(2,5);
 console.log(result);
+
+
+// typecscript code 
+type emp = {
+    id:number;
+    ename:string;
+     age:number;   
+}
+let person1:emp ={
+   id: 1,
+   ename:'anurag',
+   age:12
+}
+console.log(person1);
+let person2:emp ={
+   id: 2,
+   ename:'raj',
+   age:32
+}
+console.log(person2);
+function kgto(weigth:string | number):number{
+    if(typeof weigth  ==='number' ){
+        return weigth * 2.2;
+    }else{
+        return parseInt(weigth)*2.2;
+    }
+}
+const ans = kgto(23);
+console.log(ans);
+const ans = kgto(23);
+console.log(ans);
 
 
