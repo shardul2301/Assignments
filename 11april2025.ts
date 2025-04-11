7bc644e48d58235e38adab7a93303d01ef6d4ad2
shardul2301 <shardul.mane@nimapinfotech.com>
2025-04-11 19:25:56 +0530

    Update 11april2025.ts
    exercises of class interface and generic

--- 11april2025.ts
+++ 11april2025.ts
@@ -66,6 +66,98 @@ seat.A1='spiderman';
 seat.A2 = 'loki';
 seat.S23 = 'danish';
 console.log(seat);
+// Classes, Interfaces and Object-oriented Programming exercises
+// Define a class called Logger that takes the name of a file in its constructor and provides
+// a method for writing messages to that file. Don’t worry about the actual file I/O
+// operations. Just define the class with the right members.
+
+class logger {
+    constructor(public filename:string);
+
+   
+ }
+
+// Given the Person class below, create a getter for getting the full name of a person
+class Person {
+constructor(public firstName: string, public lastName: string) {}
+
+get fullname(){
+   return this.firstName + " "+ this.lastName;
+
+}
+set fullname(val:string){
+    this.firstName + " "+ this.lastName;
+}
+}
+
+// // Create a new class called Employee that extends Person and adds a new property
+// // called salary
+
+class Employee {
+    constructor(public salary :number){
+
+    }
+
+}
+class person extends Employee {
+    constructor( public name : string,private readonly salarymonth : number,salary:number){
+        super(salary);
+    }
+}
+
+let person1 = new person('spiderman',2000,24000);
+console.log(person1);
+
+person1.name = 'alice';
+
+// // +Given the data below, define an interface for representing employees:
+interface employee  {
+name: 'John Smith',
+salary: 50_000,
+address: Adress
+}
+
+interface Adress{
+ street: 'Flinders st',
+city: 'Melbourne',
+zipCode: 3144
+
+}
+function  echo<T extends number | string >(value:T):T{
+
+
+    return  value;
+}
+// echo(true);
+
+
+
+// Generics function  example 
+// proper way way to write 
+function add<T,E>(value:T,value2:E):[T,E]{
+   return [value,value2];
+
+}
+const val:[number,number] = add<number,number>(32,2);
+console.log(val);
+const ans = val[0]+val[1]
+console.log(ans)
+
+// not proper way to write 
+function addValues<T extends number | string>(a: T, b: T): T {
+  return (a + b) as T;
+}
+
+// Using with numbers
+const numResult = addValues<number>(10, 20);
+console.log("Number Sum:", numResult); // Output: 30
+
+// Using with strings
+const strResult = addValues<string>("Hello, ", "World!");
+console.log("String Join:", strResult); // Output: Hello, World!
+
+
+
 
 
 
