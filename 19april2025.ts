0c3f13db42cf6c3ecbc522c32d3616609ba3ef63
shardul2301 <shardul.mane@nimapinfotech.com>
2025-04-19 17:09:04 +0530

    Create 19april2025.ts

---
+++ 19april2025.ts
@@ -0,0 +1,58 @@
+
+let rt:any="" ;
+let aa:unknown='kfhwjf' ;
+console.log(rt.toUpperCase())
+console.log(aa.toUpperCase())
+let tp:number = aa;
+
+function fun(num1:string | number ){
+    console.log()
+}
+
+enum person {
+    small = true ,
+    cap =67,
+    up , 
+    down 
+}
+console.log(person.up);
+console.log(person.down);
+
+
+let arr : Array<number>=[1,1,1] ; 
+let len = arr.length;
+// console.log(len);
+
+
+ function avg(arr:[]){
+    const res =  arr.reduce((acc,curr)=>{
+        return acc+curr;
+    },0)
+    
+    return res ;
+
+    for(let i = 0 ; i<=len;i++ ){
+        console.log(arr[i]);
+    }
+}
+console.log(ans);
+
+console.log(ans);
+
+console.log(avg(arr)/len);
+
+function area(length:number,width:number){
+    return length*width;
+}
+console.log(area(4,2))
+let abs=23;
+console.log(typeof(abs));
+const df : [number,number,number] =[8,3,4];
+function fun(num1:number=12){
+    console.log(num1);
+}
+
+fun('dsj');
+
+
+
