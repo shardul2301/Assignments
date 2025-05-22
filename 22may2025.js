561251ec1118b4cfead29315a4ed603ef33233e5
shardul2301 <shardul.mane@nimapinfotech.com>
2025-05-22 18:47:14 +0530

    Create 22may2025.js

---
+++ 22may2025.js
@@ -0,0 +1,12 @@
+//  -- SELECT first_name,age from Customers where age <28 and  age >22 ;
+//  --select NAME,SALARY from newpp  where salary 28000 between 20000;
+//  --select * from newpp  where salary < 25000 limit 2;
+// --select * from customers order by age asc ;
+// --select status ,customer from shippings  order by  customer desc ;
+// /*select item ,amount from orders 
+// order by amount 
+// asc limit 2;  */
+// --select * from oders in ("keyboard","mouse");
+
+// SELECT item ,count(item) from orders 
+// group by  item;
